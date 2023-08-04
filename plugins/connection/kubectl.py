81a0658d77595a42f5414b327c3b33cb38388b70
gitarplayer <51920729+GitarPlayer@users.noreply.github.com>
2023-08-04 10:38:17 +0200

    try fixing bug

--- plugins/connection/kubectl.py
+++ plugins/connection/kubectl.py
@@ -233,26 +233,22 @@ class Connection(ConnectionBase):
             self._file_to_delete = None
 
     def _build_exec_cmd(self, cmd):
-        """Build the local kubectl exec command to run cmd on remote_host"""
+    """Build the local kubectl exec command to run cmd on remote_host"""
         local_cmd = [self.transport_cmd]
         censored_local_cmd = [self.transport_cmd]
-
+    
+        # These options come after 'kubectl exec'
+        exec_options = []
+        censored_exec_options = []
+    
         # Build command options based on doc string
         doc_yaml = AnsibleLoader(self.documentation).get_single_data()
         for key in doc_yaml.get("options"):
             if key.endswith("verify_ssl") and self.get_option(key) != "":
                 # Translate verify_ssl to skip_verify_ssl, and output as string
                 skip_verify_ssl = not self.get_option(key)
-                local_cmd.append(
-                    "{0}={1}".format(
-                        self.connection_options[key], str(skip_verify_ssl).lower()
-                    )
-                )
-                censored_local_cmd.append(
-                    "{0}={1}".format(
-                        self.connection_options[key], str(skip_verify_ssl).lower()
-                    )
-                )
+                exec_options.append("{0}={1}".format(self.connection_options[key], str(skip_verify_ssl).lower()))
+                censored_exec_options.append("{0}={1}".format(self.connection_options[key], str(skip_verify_ssl).lower()))
             elif key.endswith("kubeconfig") and self.get_option(key) != "":
                 kubeconfig_path = self.get_option(key)
                 if isinstance(kubeconfig_path, dict):
@@ -261,44 +257,44 @@ class Connection(ConnectionBase):
                         json.dump(kubeconfig_path, fp)
                     kubeconfig_path = tmpfile
                     self._file_to_delete = tmpfile
-
+    
                 cmd_arg = self.connection_options[key]
-                local_cmd += [cmd_arg, kubeconfig_path]
-                censored_local_cmd += [cmd_arg, kubeconfig_path]
+                exec_options += [cmd_arg, kubeconfig_path]
+                censored_exec_options += [cmd_arg, kubeconfig_path]
             elif (
                 not key.endswith("container")
                 and self.get_option(key)
                 and self.connection_options.get(key)
             ):
                 cmd_arg = self.connection_options[key]
-                local_cmd += [cmd_arg, self.get_option(key)]
+                exec_options += [cmd_arg, self.get_option(key)]
                 # Redact password and token from console log
                 if key.endswith(("_token", "_password")):
-                    censored_local_cmd += [cmd_arg, "********"]
+                    censored_exec_options += [cmd_arg, "********"]
                 else:
-                    censored_local_cmd += [cmd_arg, self.get_option(key)]
-
+                    censored_exec_options += [cmd_arg, self.get_option(key)]
+    
         extra_args_name = "{0}_extra_args".format(self.transport)
         if self.get_option(extra_args_name):
-            local_cmd += self.get_option(extra_args_name).split(" ")
-            censored_local_cmd += self.get_option(extra_args_name).split(" ")
-
+            exec_options += self.get_option(extra_args_name).split(" ")
+            censored_exec_options += self.get_option(extra_args_name).split(" ")
+    
         pod = self.get_option("{0}_pod".format(self.transport))
         if not pod:
             pod = self._play_context.remote_addr
         # -i is needed to keep stdin open which allows pipelining to work
         local_cmd += ["exec", "-i", pod]
         censored_local_cmd += ["exec", "-i", pod]
-
+    
         # if the pod has more than one container, then container is required
         container_arg_name = "{0}_container".format(self.transport)
         if self.get_option(container_arg_name):
-            local_cmd += ["-c", self.get_option(container_arg_name)]
-            censored_local_cmd += ["-c", self.get_option(container_arg_name)]
-
-        local_cmd += ["--"] + cmd
-        censored_local_cmd += ["--"] + cmd
-
+            exec_options += ["-c", self.get_option(container_arg_name)]
+            censored_exec_options += ["-c", self.get_option(container_arg_name)]
+    
+        local_cmd += ["--"] + cmd + exec_options
+        censored_local_cmd += ["--"] + cmd + censored_exec_options
+    
         return local_cmd, censored_local_cmd
 
     def _connect(self, port=None):
